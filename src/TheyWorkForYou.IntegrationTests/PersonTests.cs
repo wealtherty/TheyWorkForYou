7d4785a5be0e95e9abfa70637f0159d75d4d88b4
Wealtherty <yw8cq3@pm.me>
2023-10-02 10:23:37 +0100

    Introduced Nadia Whittome test

--- src/TheyWorkForYou.IntegrationTests/PersonTests.cs
+++ src/TheyWorkForYou.IntegrationTests/PersonTests.cs
@@ -13,19 +13,13 @@ public class PersonTests
     }
 
     [Fact]
-    public async Task Person_should_not_be_null()
+    public async Task Get_Person_should_return_Nadia_Whittome()
     {
         var person = await _client.GetPersonAsync(TestData.NadiaWhittome.PersonId);
         person.Should().NotBeNull();
-    }
-
-    [Fact]
-    public async Task Id_should_be_populated()
-    {
-        var person = await _client.GetPersonAsync(TestData.NadiaWhittome.PersonId);
         person.Id.Should().Be(TestData.NadiaWhittome.PersonId);
     }
-
+    
     private static class TestData
     {
         public static class NadiaWhittome
