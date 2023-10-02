3ad7db39fef5fb52f3600b600408b5682d541122
Wealtherty <yw8cq3@pm.me>
2023-10-02 10:02:08 +0100

    Added first test to kick the tires

---
+++ src/TheyWorkForYou.IntegrationTests/ClientTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TheyWorkForYou.IntegrationTests;
+
+public class ClientTests
+{
+    [Fact]
+    public async Task Get_bool_should_be_true()
+    {
+        var client = new Client();
+        var result = await client.GetBoolAsync();
+        result.Should().BeTrue();
+    }
+}
---
+++ src/TheyWorkForYou/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace TheyWorkForYou;
+
+public class Client
+{
+    public Task<bool> GetBoolAsync()
+    {
+        return Task.FromResult(true);
+    }
+}
